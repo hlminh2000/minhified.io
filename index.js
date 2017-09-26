7625c508ccc8a081adfd272f9e369f1968df94b5
hlminh2000 <hlminh2000@gmail.com>
2017-09-26 22:38:00 +0000

    adds server logger

--- index.js
+++ index.js
@@ -2,27 +2,32 @@ const   express = require('express')
         http = require('http')
         bodyParser = require('body-parser')
         nodemailer = require('nodemailer')
+        logger = require("./services/logger.js")
 
 const app = express()
 const server = http.createServer(app);
+
+app.use('*', logger.endPointLog)
 
 app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 app.use("/", express.static('client/landing/public'))
 
-app.use('*', (res, req, next) => {
-    console.log("HIT!")
-    next()
-})
+
 
 app.post('/email', function(req, res){
-    console.log(req.body)
     res.redirect('/')
 })
+
+app.get('/yo', (req, res) => {
+    res.send("as;gsdfb")
+})
+
 
 server.listen(process.env.PORT || 50000, process.env.IP || "0.0.0.0", function () {
     console.log('https://landing-hlminh2000.c9users.io/')
     console.log('Example app listening at: ', 
         process.env.IP || "0.0.0.0",
-        process.env.PORT || 50000)
+        process.env.PORT || 50000
+    )
 })
---
+++ services/logger.js
@@ -0,0 +1,22 @@
+const   winston = require('winston')
+        moment = require("moment")
+        
+const logTimestamp = () => {
+    return moment().format('YYYY-MM-DD_hh:mm:ss')
+}
+
+const logger = new winston.Logger({
+    levels: winston.config.syslog.levels,
+    colors: winston.config.syslog.colors,
+});
+
+logger.add(winston.transports.Console);
+
+logger.info(logTimestamp(), 'Logger setup')
+
+module.exports = {
+    endPointLog: (req, res, next) => {
+        logger.info(logTimestamp(), req.method, req.baseUrl)
+        next()
+    }
+}
